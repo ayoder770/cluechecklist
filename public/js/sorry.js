a4902c1905969357d1f35d03ca1cd191b6c4dea9
ayoder770 <30388381+ayoder770@users.noreply.github.com>
2017-11-23 09:45:58 -0500

    O

--- public/js/sorry.js
+++ public/js/sorry.js
@@ -1,1 +1,12 @@
+ var  desc_1 = "Move from Start of move forward 1.";
+ var  desc_2 = "Move from Start or move forward 2. DRAW AGIN";
+ var  desc_3 = "Move forward 3.";
+ var  desc_4 = "Move backward 4.";
+ var  desc_5 = "Move forward 5.";
+ var  desc_7 = "Move forward 7 or split between two pawns.";
+ var  desc_8 = "Move forward 8.";
+ var desc_10 = "Move forward 10 or move backward 1.";
+ var desc_11 = "Move forward 11 or change places with an opponent.";
+ var desc_12 = "Move forward 12.";
+ var  desc_S = "Move from Start and switch places with an opponent, who you bump back to start.";
  
