7eb81481b3ba87d30be134dd0f0b057f1ec9abab
ayoder770 <30388381+ayoder770@users.noreply.github.com>
2017-11-23 10:23:37 -0500

    T

--- public/js/sorry.js
+++ public/js/sorry.js
@@ -1,25 +1,14 @@
- var  desc_1 = "Move from Start of move forward 1.";
- var  desc_2 = "Move from Start or move forward 2. DRAW AGIN";
- var  desc_3 = "Move forward 3.";
- var  desc_4 = "Move backward 4.";
- var  desc_5 = "Move forward 5.";
- var  desc_7 = "Move forward 7 or split between two pawns.";
- var  desc_8 = "Move forward 8.";
- var desc_10 = "Move forward 10 or move backward 1.";
- var desc_11 = "Move forward 11 or change places with an opponent.";
- var desc_12 = "Move forward 12.";
- var  desc_50 = "Move from Start and switch places with an opponent, who you bump back to start.";
+ var  desc= [ "Move from Start of move forward 1.", "Move from Start or move forward 2. DRAW AGIN", "Move forward 3.", "Move backward 4.", "Move forward 5.", "Move forward 7 or split between two pawns.", "Move forward 8.", "Move forward 10 or move backward 1.", "Move forward 11 or change places with an opponent.", "Move forward 12.", "Move from Start and switch places with an opponent, who you bump back to start." ]
 
 var deck = [1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 50]
  
 function draw_next_card(){
     var x = Math.floor((Math.random() * 11) + 0);
-   var c = deck[x];
-    var next = "desc_"+c;
-    document.getElementById("top_numb").innerHTML = c;
-    document.getElementById("big_numb").innerHTML = c;
-    document.getElementById("bot_numb").innerHTML = c;
+
+    document.getElementById("top_numb").innerHTML = deck[x];
+    document.getElementById("big_numb").innerHTML = deck[x];
+    document.getElementById("bot_numb").innerHTML = deck[x];
     
-    document.getElementById("top_desc").innerHTML = 'desc_'+c;
-    document.getElementById("bot_desc").innerHTML = 'desc_'+c;
+    document.getElementById("top_desc").innerHTML = desc[x];
+    document.getElementById("bot_desc").innerHTML = desc[x];
 }
