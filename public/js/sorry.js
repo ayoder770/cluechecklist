f0a66e9654d72446f5be6b3d4ae0b362c5c876ca
ayoder770 <30388381+ayoder770@users.noreply.github.com>
2017-11-24 15:26:14 -0500

    Oscar

--- public/js/sorry.js
+++ public/js/sorry.js
@@ -1,6 +1,6 @@
-var deck = [1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 50, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 50, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 50, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 50, 1];
+var deck = [1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1];
 
-var  desc= [ "Move from Start or move forward 1.", "Move from Start or move forward 2. DRAW AGAIN", "Move forward 3.", "Move backward 4.", "Move forward 5.", "Move forward 7 or split between two pawns.", "Move forward 8.", "Move forward 10 or move backward 1.", "Move forward 11 or change places with an opponent.", "Move forward 12.", "Move from Start and switch places with an opponent, who you bump back to start." ];
+var  desc= [ "Move from Start or move forward 1.", "Move from Start or move forward 2. DRAW AGAIN", "Move forward 3.", "Move backward 4.", "Move forward 5.","NULL", "Move forward 7 or split between two pawns.", "Move forward 8.","NULL", "Move forward 10 or move backward 1.", "Move forward 11 or change places with an opponent.", "Move forward 12.", "Move from Start and switch places with an opponent, who you bump back to start." ];
 
 var one_deck = 45;
 var count_up = 0;
@@ -20,25 +20,25 @@ function draw_next_card(){
 
 
  function changeText(){
-    var x = Math.floor((Math.random() * 44) + 0);
+    var x = Math.floor((Math.random() * (deck.length)) + 0);
      
      
 
      
-    if( deck[x] === 50 ){
+    if( deck[x] === 13 ){
         document.getElementById("top_numb").innerHTML = "<div class='sorry'>SORRY!</div>";
         document.getElementById("big_numb").innerHTML = "";
         document.getElementById("bot_numb").innerHTML = "<div class='sorry'>SORRY!</div>"
     
-        document.getElementById("top_desc").innerHTML = desc[deck.length - 1];
-        document.getElementById("bot_desc").innerHTML = desc[deck.length - 1];   
+        document.getElementById("top_desc").innerHTML = desc[12];
+        document.getElementById("bot_desc").innerHTML = desc[12];   
     } else{
         document.getElementById("top_numb").innerHTML = deck[x];
         document.getElementById("big_numb").innerHTML = deck[x];
         document.getElementById("bot_numb").innerHTML = deck[x];
     
-        document.getElementById("top_desc").innerHTML = desc[x];
-        document.getElementById("bot_desc").innerHTML = desc[x];
+        document.getElementById("top_desc").innerHTML = desc[deck[x-1]];
+        document.getElementById("bot_desc").innerHTML = desc[deck[x-1]];
     }
      
      update_deck(x);
