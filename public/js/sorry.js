289a7367cc326829dcdbf690cdb3077569915d01
ayoder770 <30388381+ayoder770@users.noreply.github.com>
2017-11-24 09:59:43 -0500

    Romeo

--- public/js/sorry.js
+++ public/js/sorry.js
@@ -5,6 +5,8 @@ var deck = [1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 50]
 function draw_next_card(){
     var x = Math.floor((Math.random() * 11) + 0);
 
+    document.getElementById("card").classList.toggle('flipped');
+  /*  
 var check = document.getElementById("card").classList.contains('flipped');
     if(check === false){
         document.getElementById("card").className = "flipped";
@@ -12,7 +14,7 @@ var check = document.getElementById("card").classList.contains('flipped');
         document.getElementById("card").className.remove = "flipped";
     }
 
-
+*/
     if(x == (deck.length - 1)){
         document.getElementById("top_numb").innerHTML = "<div style='text-align:center;'>SORRY!</div>";
         document.getElementById("big_numb").innerHTML = "";
