ce1ad16de4ffc77085005620e61ec9e549c7be50
ayoder770 <30388381+ayoder770@users.noreply.github.com>
2017-11-24 15:58:22 -0500

    Uniform

--- public/js/sorry.js
+++ public/js/sorry.js
@@ -1,10 +1,7 @@
 var deck = [1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1];
 
 var  desc= [ "NULL", "Move from Start or move forward 1.", "Move from Start or move forward 2. DRAW AGAIN", "Move forward 3.", "Move backward 4.", "Move forward 5.","NULL", "Move forward 7 or split between two pawns.", "Move forward 8.", "NULL", "Move forward 10 or move backward 1.", "Move forward 11 or change places with an opponent.", "Move forward 12.", "Move from Start and switch places with an opponent, who you bump back to start." ];
-
-//1 - 5, all else - 4...
- 
-
+s
 function draw_next_card(){
     if(deck.length === -1){
          document.getElementById("card").classList.toggle('flipped_half'); 
@@ -21,9 +18,6 @@ function draw_next_card(){
  function changeText(){
     var x = Math.floor((Math.random() * (deck.length)) + 0);
      
-     
-
-     
     if( deck[x] === 13 ){
         document.getElementById("top_numb").innerHTML = "<div class='sorry'>SORRY!</div>";
         document.getElementById("big_numb").innerHTML = "";
@@ -39,16 +33,9 @@ function draw_next_card(){
         document.getElementById("top_desc").innerHTML = desc[deck[x]];
         document.getElementById("bot_desc").innerHTML = desc[deck[x]];
     }
-     
-     update_deck(x);
+     deck.splice(index, 1);
 }
 
-
-function update_deck(index){
-    
-    deck.splice(index, 1);
-
-}
 
 function shuffle_deck(){
     alert("DECK IS EMPTY...RESHUFFLE");
