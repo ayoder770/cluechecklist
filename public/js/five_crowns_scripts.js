6e4ec1d2940c2ff5d3a9928bdce83201ed088d46
ayoder770 <30388381+ayoder770@users.noreply.github.com>
2017-11-25 15:25:40 -0500

    Juliette

--- public/js/five_crowns_scripts.js
+++ public/js/five_crowns_scripts.js
@@ -1,45 +1,41 @@
  var this_round = 3;
 
 $(function(){
-            $('#score_form').on('submit', function(e){
-            e.preventDefault();
-              //   var P1_score = document.getElementById("P_1").value;
-              //   var P2_score = document.getElementById("P_2").value;
-              //   var P3_score = document.getElementById("P_3").value;
-              //   var P4_score = document.getElementById("P_4").value;
-              //   var P5_score = document.getElementById("P_5").value;
-         
-//alert(P1_score);
-              //  alert(P2_score);
-
-               document.getElementById("P1").innerHTML = "Andrew";
+    $('#score_form').on('submit', function(e){
+        e.preventDefault();
                 
-                for(var i=0; i<5; i++){
+            for(var i=0; i<5; i++){
                    
-                    var s_new_cell;
-                    s_new_cell = 'p' + (i+1) + '_' + this_round + '_n';
-                    var s_upd = 'p' + (i+1) + "_" + this_round + "_u";
-                 //   alert(s_new);
+                // SET UP VARIABLE FOR NEW SCORE CELL
+                var s_new_cell;
+                s_new_cell = 'p' + (i+1) + '_' + this_round + '_n';
                     
-                    
-                    var s_get;
-                    s_get = 'P_'+(i+1);
-                   // alert(s_get);
-                
-                    document.getElementById(s_new_cell).innerHTML = document.getElementById(s_get).value;
-                    
-                    
-                  //  $(s_new).innerHTML = "P"+(i+1)+"_score";
-                    
-                    
-                  //  $(s_new).innerHTML = "P";
-                    
-                    
-                }
+                // SET UP VARIABLE FOR UPDATE SCORE CELL
+                var s_upd_cell;
+                s_upd_cell = 'p' + (i+1) + "_" + this_round + "_u";
+                 
+                // SET UP VARIABLE FOR FORM SCORES
+                var s_get;
+                s_get = 'P_'+(i+1);
+                     
+                document.getElementById(s_new_cell).innerHTML = document.getElementById(s_get).value;
                 
                 
-                
-      
-});
-                   
+                 if(this_round === 3 ){
+                     document.getElementById(s_upd_cell).innerHTML = document.getElementById(s_get).value;
+                 } else{
+                     
+                     // SET UP VARIABLE FOR PREVIOUS ROUNDS TOTAL SCORE
+                     var s_old_total;
+                     s_old_total = s_upd_cell = 'p' + (i+1) + "_" + (this_round - 1) + "_u";
+                     
+                     
+                     document.getElementById(s_upd_cell).innerHTML = (document.getElementById(s_get).value) + (document.getElementById(s_old_total).value);
+                 }   
+                 
+                    
+                    
+            }
+        this_round++;
+    });                 
 }); 
