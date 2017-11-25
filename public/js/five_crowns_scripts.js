90946b1d7f04bad27b0813b46c78b07c8b478957
ayoder770 <30388381+ayoder770@users.noreply.github.com>
2017-11-25 14:06:03 -0500

    Juliette

---
+++ public/js/five_crowns_scripts.js
@@ -0,0 +1,11 @@
+ $(function(){
+            $('#score_form').on('submit', function(e){
+            e.preventDefault();
+          //  var players = $('#players').val();
+                console.log("Hello");
+
+               document.getElementById("P1").innerHTML = "hello";
+      
+        });
+                   
+        }); 
