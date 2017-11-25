114269270aa04d5aa18fcd3747e59534a0fc4781
ayoder770 <30388381+ayoder770@users.noreply.github.com>
2017-11-25 14:26:41 -0500

    Kilo

--- public/js/five_crowns_scripts.js
+++ public/js/five_crowns_scripts.js
@@ -1,11 +1,33 @@
- $(function(){
+ var this_round = 3;
+
+$(function(){
             $('#score_form').on('submit', function(e){
             e.preventDefault();
-          //  var players = $('#players').val();
+                 var P1_score = $("P_1").val();
+                 var P2_score = $("P_2").val();
+                 var P3_score = $("P_3").val();
+                 var P4_score = $("P_4").val();
+                 var P5_score = $("P_5").val();
+         
                 console.log("Hello");
 
-               document.getElementById("P1").innerHTML = "hello";
+               document.getElementById("P1").innerHTML = "Andrew";
+                
+                for(var i=0 i<5 i++){
+                    var one = "#p";
+                    var s_new = one + (i+1) + "_" + this_round + "_n";
+                    var s_upd = one + (i+1) + "_" + this_round + "_u";
+                    $(s_new).innerHTML = "P"+(i+1)+"_score";
+                    
+                    
+                    
+                    
+                    
+                }
+                
+                
+                
       
-        });
+});
                    
-        }); 
+}); 
