f182b43ed13a80ca4de5f782171a999f16a69d7c
ayoder770 <30388381+ayoder770@users.noreply.github.com>
2018-01-06 15:51:48 -0500

    India

--- public/js/taboo_scripts.js
+++ public/js/taboo_scripts.js
@@ -2,6 +2,30 @@ var a_pts = 0;
 var b_pts = 0;
 var whosup;
 
-function add_a(){
-    document.getElementById("team_a_pts").innerHTML = Number(a_pts+1);
+function team_a_turn(){
+    
+}
+
+function team_b_turn(){
+    
+}
+
+function begin_game(){
+    
+}
+
+function add_pts(){
+    console.log("hello from add points");
+}
+
+function sub_pts(){
+    console.log("hello from sub points");
+}
+
+function taboo(){
+    console.log("hello from taboo");
+}
+
+function pass(){
+    console.log("hello from pass");
 }
