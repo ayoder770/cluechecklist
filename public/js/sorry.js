56959fa42f2cbfb6b530f79e6048f5e09d316976
ayoder770 <30388381+ayoder770@users.noreply.github.com>
2019-07-19 21:08:49 -0400

    Bravo

--- public/js/sorry.js
+++ public/js/sorry.js
@@ -1,21 +1,33 @@
+// JavaScript For Sorry deck
+
+// Initialize the deck
 var deck = [1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1];
 
+// Full Deck
+var deckFull = 45;
+
+// Array to hold the description/instructions of each card
 var  desc= [ "NULL", "Move from Start or move forward 1.", "Move from Start or move forward 2.<br /><b>DRAW AGAIN</b>", "Move forward 3.", "Move backward 4.", "Move forward 5.","NULL", "Move forward 7 or split between two pawns.", "Move forward 8.", "NULL", "Move forward 10 or move backward 1.", "Move forward 11 or change places with an opponent.", "Move forward 12.", "Move from Start and switch places with an opponent, who you bump back to start." ];
 
 // Function when card is tapped to draw the next card
 function draw_next_card(){
-    if(deck.length === 45){
+    
+    // Full deck
+    if(deck.length === deckFull){
          document.getElementById("card").className = "flipped_half"; 
          setTimeout(changeText, 300);
-     } else if( ( deck.length > 0 ) && ( deck.length < 45 ) ){
+        
+    // Deck is not full but not empty
+    } else if( ( deck.length > 0 ) && ( deck.length < deckFull ) ){
          document.getElementById("card").classList.remove('flipped_half');
          document.getElementById("card_front").style.transform = "rotateY( 0deg )";
          document.getElementById("card_back").style.transform = "rotateY( 180deg )";
          document.getElementById("card").classList.toggle('flipped');
-        // setTimeout(changeText, 300);
-     } else if( deck.length === 0 ){
+         setTimeout(changeText, 300);
+    } else if( deck.length === 0 ){
          document.getElementById("rs_overlay").style.display = "block";
-     }
+    }
+    
 }
 
 
@@ -41,10 +53,13 @@ function changeText(){
         document.getElementById("top_desc").innerHTML = desc[deck[x]];
         document.getElementById("bot_desc").innerHTML = desc[deck[x]];
     }
-     deck.splice(x, 1);
+    
+    // Remove the card that was drawn from the desk
+    deck.splice(x, 1);
 }
 
 
+// Function to shuffle deck (reset cards)
 function shuffle_deck(){
      deck = [1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 1];
      document.getElementById("rs_overlay").style.display = "none";
