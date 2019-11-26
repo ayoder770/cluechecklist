eab8a64f0eb1cf3e4ec07fff641f8c79226b2bf8
ayoder770 <30388381+ayoder770@users.noreply.github.com>
2019-11-26 09:18:14 -0500

    Foxtrot

--- public/js/clue_scripts.js
+++ public/js/clue_scripts.js
@@ -61,57 +61,16 @@
         }
     });
     
-// Function to disable "pull-to-refresh" effect present in some webviews.
-// Especially Crosswalk 12 and above (Chromium 41+) runtimes.
+const target = window 
+let lastY = 0
 
-window.addEventListener('load', function() {
-    var lastTouchY = 0 ;
-    var maybePreventPullToRefresh = false ;
+target.addEventListener('touchmove', handleTouchMove)
 
-    // Pull-to-refresh will only trigger if the scroll begins when the
-    // document's Y offset is zero.
-
-    var touchstartHandler = function(e) {
-        if( e.touches.length != 1 ) {             
-            return ;
-        }
-        lastTouchY = e.touches[0].clientY ;
-        // maybePreventPullToRefresh = (preventPullToRefreshCheckbox.checked) && (window.pageYOffset == 0) ;
-        maybePreventPullToRefresh = (window.pageYOffset === 0) ;
-        //document.getElementById('txtLog').textContent = "maybePreventPullToRefresh: " + maybePreventPullToRefresh;
-    };
-
-    // To suppress pull-to-refresh it is sufficient to preventDefault the
-    // first overscrolling touchmove.
-
-    var touchmoveHandler = function(e) {
-        var touchY = e.touches[0].clientY ;
-        var touchYDelta = touchY - lastTouchY ;
-        lastTouchY = touchY ;
-
-        if (maybePreventPullToRefresh) {
-            maybePreventPullToRefresh = false ;
-            //if (touchYDelta > 0) {
-                e.preventDefault() ;
-                //document.getElementById('txtLog').textContent = "TouchY: " + touchYDelta;
-                // console.log("pull-to-refresh event detected") ;
-                return ;
-            //}
-        }
-
-        // if (preventScrollCheckbox.checked) {
-        //     e.preventDefault() ;
-        //     return ;
-        // }
-
-        // if (preventOverscrollGlowCheckbox.checked) {
-        //     if (window.pageYOffset == 0 && touchYDelta > 0) {
-        //         e.preventDefault() ;
-        //         return ;
-        //     }
-        // }
-    };
-
-    document.addEventListener('touchstart', touchstartHandler, false) ;
-    document.addEventListener('touchmove', touchmoveHandler, false) ;
-}) ;
+function handleTouchMove(e) {
+  const { pageY } = e.changedTouches[0]
+  const scrollY = target.pageYOffset || target.scrollTop || 0
+  if (pageY > lastY && scrollY === 0) {
+    e.preventDefault()
+  }
+  lastY = pageY
+});
