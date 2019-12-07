6a49dc45b1cb514e2ddb61b11738341d4c8b7c49
ayoder770 <30388381+ayoder770@users.noreply.github.com>
2019-12-07 14:55:30 -0500

    Alpha

--- public/js/five_crowns_scripts.js
+++ public/js/five_crowns_scripts.js
@@ -1,11 +1,64 @@
 var current = 3;
+var numberOfPlayers;
 
+// Function to read number of players and build the form dynamically
+$(function(){
+    $('#player_number_form').on('submit', function(e){
+        e.preventDefault();
+        
+    // Turn off the form as it is no longer needed
+    document.getElementById("player_number_form").classList.toggle('display_block');
+    document.getElementById("player_number_form").classList.toggle('display_none');
+        
+    // Set the number of players based on user input
+    numberOfPlayers = document.getElementById("number_of_players").value;
+        
+    //Build the player name header
+    var scoreHTML = "<div class='full_row'><div class='round'></div>";
+    for(var i=1; i<= numberOfPlayers; i++){
+        scoreHTML+="<div class='player' id='P"+i+"'>Plyr. "+i+"</div>";
+    }
+    scoreHTML+="</div>";
+        
+    // Build the entire scorecard
+    // Outer loop for number of rounds in a five crowns game
+    for(var j=3; j<=13; j++){
+        scoreHTML+="<div id='row_round_"+j+"' class='full_row'><div id='round_"+j+"' class='fc_round'>"+j+"</div>";
+        // Inner Loop based on how many players are playing
+        for(var k=1; k<=numberOfPlayers; k++){
+            scoreHTML+="<div class='score_cont'><div class='this_score' id='p"+k+"_"+j+"_n'></div><div class='this_update' id='p"+k+"_"+j+"_u'></div></div>"
+        }
+        scoreHTML+="</div>";  
+    }
+        
+    // Populate HTML for the name form
+    var nameFormHTML;
+    for(var l=1; l<=numberOfPlayers; l++){
+        nameFormHTML+="<input class='name_input' id='PN_"+l+"' type='text' placeholder='Player "+l+"'/><br>";
+    }
+        
+    // Set the HTML for the name form
+    document.getElementById("name_form_input").innerHTML=nameFormHTML;
+        
+    // Set the form HTML on the page 
+    document.getElementById("scorecard_master_container").innerHTML=scoreHTML;
+       
+    // Enable the name form to gather player's names
+    document.getElementById("name_form_cont").classList.toggle('display_block');
+    document.getElementById("name_form_cont").classList.toggle('display_none');
+        
+    });                 
+}); 
+ 
+
+// Function to add player's names to scorecard
 $(function(){
     $('#name_form').on('submit', function(e){
         e.preventDefault();
-        document.getElementById("name_form_cont").style.display = "none";
+        document.getElementById("name_form_cont").classList.toggle('display_block');
+        document.getElementById("name_form_cont").classList.toggle('display_none');
         
-        for(var i=0; i<5; i++){
+        for(var i=0; i<numberOfPlayers; i++){
             
             var fname;
             fname = "PN_"+(i+1);
@@ -26,140 +79,124 @@ $(function(){
             
             document.getElementById(fname).value = '';
         } 
-        
     });                 
 }); 
 
+
+// Function to update player's score
 function column_update(round_number, player, score){
     console.log("Round "+round_number+" | Player "+player+" | Score "+score);
     
-    // SET UP VARIABLE FOR "THIS ROUND" SCORE CELL
-    var s_new_cell;
-    s_new_cell = 'p' + (player+1) + '_' + round_number + '_n';
+    // ID of cell for player's score this round
+    var s_new_cell = 'p'+player+'_'+round_number+'_n';
                     
-    // SET UP VARIABLE FOR UPDATE SCORE CELL
-    var s_upd_cell;
-    s_upd_cell = 'p' + (player+1) + "_" + round_number + "_u";
+    // ID of cell for player's cumulative score after this round
+    var s_upd_cell = 'p'+player+'_'+round_number+'_u';
     
+    // If not the first (third) round, need to know the current cumulative score for player
     if(round_number != 3){
-        // SET UP VARIABLE FOR PREVIOUS ROUNDS TOTAL SCORE
+        // ID of cell holdin player's current cumulative score from last round
         var s_old_total;
-        s_old_total = 'p' + (player+1) + "_" + (round_number - 1) + "_u";
+        s_old_total = 'p'+player+'_'+(round_number - 1)+'_u';
     }
     
-    // IF THIS IS NOT A CORRECTION
+    // If this is not a correction, update the score for current round
     if(round_number == current){
-        // ENTER SCORE FOR THIS ROUND
-        document.getElementById(s_new_cell).innerHTML = score;
+        
+        // Enter the player's score for this round
+        document.getElementById(s_new_cell).innerHTML = Number(score);
         
         if(round_number == 3 ){
-            // ENTER SAME SCORE SINCE THIS IS FIRST ROUND
+            // Cumulative score is the same as round score since round one
             document.getElementById(s_upd_cell).innerHTML = Number(score);
         } else{
-            // ADD TO PREVIOUS ROUND SCORE         
+            // Add this round score to cumulative score         
             document.getElementById(s_upd_cell).innerHTML = Number(score) + Number(document.getElementById(s_old_total).innerHTML);
         }   
+    // If not a score correction, this player is all set. Move onto next player
         
-        
-        
-        
-    // THIS IS A CORRECTION
+    // Else this is a correction
     } else{
+        // Enter the corrected round score 
+        document.getElementById(s_new_cell).innerHTML = Number(score);
         
-        // ENTER CORRECTED SCORE
-        document.getElementById(s_new_cell).innerHTML = score;
+        // Start with round 3, set round equal to cumulative
+        var roundThreeScoreID = 'p'+player+'_3_n';
+        var roundThreeCumulID = 'p'+player+'_3_u';
+        document.getElementById(roundThreeCumulID).innerHTML = Number(document.getElementById(roundThreeScoreID).innerHTML);
         
-        for( var i=round_number; i<current; i++){
-            // SET UP VARIABLE FOR "THIS ROUND" SCORE CELL
-            var s_new_cell;
-            s_new_cell = 'p' + (player+1) + '_' + i + '_n';
-                    
-            // SET UP VARIABLE FOR UPDATE SCORE CELL
-            var s_upd_cell;
-            s_upd_cell = 'p' + (player+1) + "_" + i + "_u";
-    
-            if(round_number != 3){
-                // SET UP VARIABLE FOR PREVIOUS ROUNDS TOTAL SCORE
-                var s_old_total;
-                s_old_total = 'p' + (player+1) + "_" + (i - 1) + "_u";
-            }
-
-        
-        if( i == 3 ){
-            // ENTER SAME SCORE SINCE THIS IS FIRST ROUND
-            document.getElementById(s_upd_cell).innerHTML = Number(score);
-        } else{
-            // ADD TO PREVIOUS ROUND SCORE         
-            document.getElementById(s_upd_cell).innerHTML = Number(score) + Number(document.getElementById(s_old_total).innerHTML);
-        }   
+        // Loop through player's scorecard to update cumulative score
+        for( var i=3; i<(current-1); i++){
             
+            // Cell ID for last cumulative score
+            var lastCumulScore = 'p'+player+'_'+i+'_u';
+            // Cell ID for round score of next round
+            var nextRoundScore = 'p'+player+'_'+(i+1)+'_n';
+            // Cell ID for cumulative score after next round
+            var nextRoundCumul = 'p'+player+'_'+(i+1)+'_u';
+            
+            // Update Score
+            document.getElementById(nextRoundCumul).innerHTML = Number(document.getElementById(lastCumulScore).innerHTML) + Number(document.getElementById(nextRoundScore).innerHTML);
         
-    } 
+        } 
+    }
 }
 
-}
 
-
-  
+// Submit the score form
 $(function(){
     $('#score_form').on('submit', function(e){
         e.preventDefault();
-        document.getElementById("score_form").classList.toggle('toggle_form');
-        var this_round = document.getElementById("hid_round").value;
-        console.log("thisround "+ this_round);
-        console.log("This round is: "+current);
-            for(var i=0; i<5; i++){
-            /*    column_update(this_round, i);   
-                // SET UP VARIABLE FOR NEW SCORE CELL
-                var s_new_cell;
-                s_new_cell = 'p' + (i+1) + '_' + this_round + '_n';
-                    
-                // SET UP VARIABLE FOR UPDATE SCORE CELL
-                var s_upd_cell;
-                s_upd_cell = 'p' + (i+1) + "_" + this_round + "_u";
-                 console.log(s_upd_cell);*/
-                // SET UP VARIABLE FOR FORM SCORES
-                var s_get;
-                s_get = 'P_'+(i+1);
-                     
-            //    document.getElementById(s_new_cell).innerHTML = document.getElementById(s_get).value;
-                var score;
-                score = document.getElementById(s_get).value;
-             //   console.log(score);
-                if( score != ""){
-                    column_update(this_round, i, score);
-                }
-                /*
-                 if(this_round == 3 ){
-                     document.getElementById(s_upd_cell).innerHTML = document.getElementById(s_get).value;
-                 } else{
-                     
-                     // SET UP VARIABLE FOR PREVIOUS ROUNDS TOTAL SCORE
-                     var s_old_total;
-                     s_old_total = 'p' + (i+1) + "_" + (this_round - 1) + "_u";
-                     
-                     document.getElementById(s_upd_cell).innerHTML = Number(document.getElementById(s_get).value) + Number(document.getElementById(s_old_total).innerHTML);
-                 }  */ 
-                       
-            }
-        $('#score_form').children('input').val('')
-        if((this_round < 13) && (this_round == current)){
-            var this_row;
-            var next_row;
-            this_row = "row_round_"+this_round;
-            next_row = "row_round_"+(Number(this_round) + 1);
-            document.getElementById(this_row).style.backgroundColor = "#FFFFFF";
-            document.getElementById(next_row).style.backgroundColor = "#C494D2";
-            current++;
-      //      console.log(current);
+        
+    // Hide the form
+    document.getElementById("score_form_cont").classList.toggle('display_block');
+    document.getElementById("score_form_cont").classList.toggle('display_none');
+     
+    // Get the round number of form being submitted
+    var this_round = document.getElementById("hid_round").value;
+    console.log("Score update for round: "+this_round);
+        
+    // Loop over all players for score updates
+    for(var i=1; i<=numberOfPlayers; i++){
+            
+        // ID Variable to read scores out of submitted form
+        var formPlayerID = 'P_'+i;
+                
+        // Populate the score for the player
+        var playerScore = document.getElementById(formPlayerID).value;
+             
+        // If score is not empty, update player's score
+        if( playerScore != ""){
+            column_update(this_round, i, playerScore);
         }
-    });                 
+               
+                       
+    }
+      
+    // Remove values from the score form for next round
+    $('#score_form').children('input').val('')
+        
+    if((this_round < 13) && (this_round == current)){
+        var this_row;
+        var next_row;
+        this_row = "row_round_"+this_round;
+        next_row = "row_round_"+(Number(this_round) + 1);
+        document.getElementById(this_row).style.backgroundColor = "#FFFFFF";
+        document.getElementById(next_row).style.backgroundColor = "#C494D2";
+        current++;
+    }
+        
+    });    
 }); 
 
-$('.fc_round').click(function(){
-    document.getElementById("score_form").classList.toggle('toggle_form');
+
+// Display the Score form 
+$('#scorecard_master_container').on("click", ".fc_round", function(){
+    document.getElementById("score_form_cont").classList.toggle('display_block');
+        document.getElementById("score_form_cont").classList.toggle('display_none');
     var this_id = $(this).attr('id');
     var splt = this_id.split("_");
     var new_id = Number(splt[1]);
     document.getElementById("hid_round").value = new_id; 
 });
+
